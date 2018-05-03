7c0d34885cd04f09ba7139bedd5c16cdd8683133
Christopher Miller <camiller.yr@gmail.com>
2018-05-03 16:29:32 -0500

    added some event listeners

--- event_listener.js
+++ event_listener.js
@@ -1,9 +1,92 @@
+// ======== Button 1 ========
+
 // find the button using the ID "alert-button"
 alertButton = document.getElementById("alert-button");
-console.log(alertButton);
 
-const buttonPress = (event) => {
-    console.log("Ive been pressed")
-}
-x = 1;
-alertButton.addEventListener("click", buttonPress)
+//define a function to be used when the button is clicked
+const buttonPress = event => {
+  alert("Ive been pressed");
+};
+
+// use add event listener to tell your javascript what to do and when to do it
+alertButton.addEventListener("click", buttonPress);
+
+// ======== Button 2 ========
+
+// find the button using the ID "change-text"
+changeButton = document.getElementById("change-button");
+
+//define a function to be used when the button is clicked
+const changeText = event => {
+  textOptions = [
+    "I have been changed!",
+    "You have changed me!",
+    "Why?!?!?!?!",
+    "Stop It!!!",
+    "Don't you dare click it again!",
+    "You Monster"
+  ];
+  changeButton.innerHTML = textOptions[Math.floor(Math.random() * 6)];
+};
+
+// use add event listener to tell your javascript what to do and when to do it
+changeButton.addEventListener("click", changeText);
+
+// ======== Button 3 & 4 ========
+
+// find the button using the ID "change-text"
+addStarButton = document.getElementById("addStar-button");
+
+//define a function to be used when the button is clicked
+const addStars = event => {
+  document.getElementById("stars").innerHTML += "*";
+};
+
+// use add event listener to tell your javascript what to do and when to do it
+addStarButton.addEventListener("click", addStars);
+
+// find the button using the ID "change-text"
+removeStarButton = document.getElementById("removeStar-button");
+
+//define a function to be used when the button is clicked
+const removeStars = event => {
+  document.getElementById("stars").innerHTML = document
+    .getElementById("stars")
+    .innerHTML.slice(0, -1);
+};
+
+// use add event listener to tell your javascript what to do and when to do it
+removeStarButton.addEventListener("click", removeStars);
+
+// ======== Input 1 & Button 5 ========
+const copyTextInput = document.getElementById("copyText-input");
+const copyTextButton = document.getElementById("copyText-button");
+
+const copyText = event => {
+  document.getElementById("copyText").innerHTML = copyTextInput.value;
+};
+
+copyTextButton.addEventListener("click", copyText);
+
+// ======== Input 2 ========
+const liveCopyInput = document.getElementById("liveCopy-input");
+
+const liveCopyText = event => {
+  document.getElementById("liveCopyText").innerHTML = liveCopyInput.value;
+};
+
+liveCopyInput.addEventListener("keyup", liveCopyText);
+
+// ======== Button 6 ========
+const hoverButton = document.getElementById("hover-button");
+
+const makeRed = event => {
+  hoverButton.style.color = "red";
+};
+
+const makeBlack = event => {
+  hoverButton.style.color = "black";
+};
+
+hoverButton.addEventListener("mouseover", makeRed);
+hoverButton.addEventListener("mouseout", makeBlack);
