58b65fe1296d00c080f58a0c1d315036a52a9847
John Dulaney <john.s.dulaney1@gmail.com>
2018-05-03 12:27:05 -0500

    :fire: what is a javascript

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
+// Array of employes set in Keyless objects
+// Each Object in the array has the following information. They all are strings.:
+// id: integer
+// name: string
+// title: string
+// age: string
+
 const employeeList = [
   {
-    id: '2',
+    id: 2,
     name: 'Chaz Vanderbilt',
     title: 'High Roller',
     age: '26',
   },
   {
-    id: '5',
+    id: 5,
     name: 'John Dulaney',
     title: 'Lowly Developer',
     age: '29',
   },
   {
-    id: '9',
+    id: 9,
     name: 'Chris Miller',
     title: 'Wizard',
     age: '32',
   },
   {
-    id: '1',
+    id: 1,
     name: 'Chazzette Belmont',
     title: 'CEO',
     age: '30',
   },
 ];
 
-const listElement = document.getElementById("array");
+// get the 'array' div and 'bookmark' it by saving it in a variable
+const listElement = document.getElementById('array');
 
+// use the previously set variable to grab the 'array' div and use JS's .innerHTML built in function to inject HTML
 listElement.innerHTML = `
-    <h3>${employeeList[1].name}</h3>
-    <br>
-    <p>${employeeList[1].title}</p>
-    <br>
-    <p>${employeeList[1].age}</p>
-    <br>
+    <div id=${employeeList[1].id} class="card">
+        <h3>${employeeList[1].name}</h3>
+            <br>
+        <p>${employeeList[1].title}</p>
+            <br>
+        <p>${employeeList[1].age}</p>
+            <br>
+    </div>
     `;
