62679263b71967a97822b790b551ed4d1d0ea524
John Dulaney <john.s.dulaney1@gmail.com>
2018-05-03 15:24:11 -0500

    :alien: yo chris wtf

--- event_listener.js
+++ event_listener.js
@@ -2,4 +2,8 @@
 alertButton = document.getElementById("alert-button");
 console.log(alertButton);
 
+const buttonPress = (event) => {
+    console.log("Ive been pressed")
+}
 x = 1;
+alertButton.addEventListener("click", buttonPress)
