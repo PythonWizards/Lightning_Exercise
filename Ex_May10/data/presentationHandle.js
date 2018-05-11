953091288dd730ab7e5320b72a0d935271885b77
John Dulaney <john.s.dulaney1@gmail.com>
2018-05-10 20:43:00 -0500

    :beers: anything changed in the data directory isnt for students. Added axios crud package

--- Ex_May10/data/presentationHandle.js
+++ Ex_May10/data/presentationHandle.js
@@ -1,38 +1,50 @@
 // This is not meant for future NSS student consumption.  It is the innerworkings of doing everything possible to avoid hard coding.
-const slideThatWeAreCurrentlyOn = document.querySelectorAll('#slides .slide');
+const Title = document.getElementsByClassName('main_title')[0];
+const Content = document.getElementsByClassName('main_content')[0];
 
-const getData = () => {
-  fetch('./data/factory.json')
-    .then(
-      (r = response => {
-        if (response.status !== 200) {
-          console.log(
-            'Looks like there was a problem. Status Code: ' + response.status
-          );
-          return;
-        }
-        response.json().then(
-          (r = data => {
-            console.log(data);
-            dom(data)
-          })
-        );
-      })
-    )
-    .catch(function(err) {
-      console.log('Fetch Error', err);
-    });
-}
+// axios Plugin method
+axios.get('./data/factory.json')
+.then(r = response => {
+  console.log(response.data.slide1);
+  dom(response)
+})
+.catch(function(error) {
+  console.log(error);
+});
 
-getData()
+// function that prints to First title slide
+const dom = response => {
+  const lazy = response.data.slide1
+  console.log(lazy);
+  Title.innerHTML = `
+    ${lazy.title}
+  `;
+  Content.innerHTML = `
+    ${lazy.content}
+  `;
+};
 
-
-
-const dom = data => {
-    slideThatWeAreCurrentlyOn.forEach(r => {
-        console.log(r.title)
-        r.innerHtml = `
-        <h1>${data.title}</h1>
-        `
-    })
-}
+// native Fetch API method
+// const getData = () => {
+//   fetch('./data/factory.json')
+//     .then(
+//       (r = response => {
+//         if (response.status !== 200) {
+//           console.log(
+//             'Looks like there was a problem. Status Code: ' + response.status
+//           );
+//           return;
+//         }
+//         response.json().then(
+//           (r = data => {
+//             console.log(data);
+//             dom(data)
+//           })
+//         );
+//       })
+//     )
+//     .catch(function(err) {
+//       console.log('Fetch Error', err);
+//     });
+// }
+// getData()
