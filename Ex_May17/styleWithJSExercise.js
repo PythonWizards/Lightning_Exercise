8b91546377bf752308b47a97857f1578da01c2f0
John Dulaney <john.s.dulaney1@gmail.com>
2018-05-16 16:27:09 -0500

    :ghost: added empty css with JS Exercise

---
+++ Ex_May17/styleWithJSExercise.js
@@ -0,0 +1,29 @@
+// Get the 'html' element using querySelector. Store it in a variable.
+
+    // set the backroundColor to 'lightyellow'
+
+// Get the 'section' element using querySelector. Store it in a variable.
+
+    // Add a '1px dashed Black' border to the stored section
+
+    // also give the section a width of '40%'
+
+    // give the main section a 'textalign' property of center
+
+    // give the main section a cursor of 'crosshair'
+
+// Get the 'sloppyClassNames' element using getElementsByClassName. Store it in a variable. Remember that the DOM will bring back an array, use brackets to pick the right element
+
+    // add a 'backgroundColor' of tomato to the class 'sloppyClassNames'
+
+// Get the 'sloppyClassNames' element using getElementsByClassName. Store it in a variable. See above instructions for dealing with this array
+    
+    // add a 'backgroundColor' of goldenrod to the class 'sloppyClassNames'
+
+// Get the 'listofWords' element using getElementById. Store it in a variable.
+    
+    // add a border of '2px solid magenta' to the unorderlist
+    
+    // add a width of '40%'
+
+// Get the 'li' elements using querySelectorAll. Store it in a variable. This returns a DOMString. We can perform methods on this DOMString as if it were an array. Here you should use a for loop to add a border of '0.5px dotted black' to each listItem.
